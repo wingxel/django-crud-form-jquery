9158cdf62c6b44f133eaf9ec9fc15059a343f9c3
wingxel <wingxel@gmail.com>
2023-05-25 14:25:43 +0300

    Created skeleton app

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request: WSGIRequest) -> HttpResponse:
+    return render(request, "app/index.html")
