4b59f788e101c7c380490cc0fe0344e7c2103e7b
wingxel <wingxel@gmail.com>
2023-05-25 15:32:05 +0300

    Created home page

--- app/views.py
+++ app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 
+from .models import MyDb
+
 
 def index(request: WSGIRequest) -> HttpResponse:
+    my_db = MyDb.objects.all()
     return render(request, "app/index.html", {
-        "test_header": "Hello, World!"
+        "user_info": my_db
     })
