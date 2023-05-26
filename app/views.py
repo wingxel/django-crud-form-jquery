95ca3137d1ed7281c73efde34234c7303a8d7175
wingxel <wingxel@gmail.com>
2023-05-26 17:04:46 +0300

    Update

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views import View
 
 from .models import MyDb
@@ -9,7 +9,6 @@ from .models import MyDb
 
 def index(request: WSGIRequest) -> HttpResponse:
     my_db = MyDb.objects.all()
-    print(my_db)
     return render(request, "app/index.html", {
         "user_info": my_db
     })
@@ -18,7 +17,6 @@ def index(request: WSGIRequest) -> HttpResponse:
 class Add(View):
     def post(self, request: WSGIRequest) -> HttpResponse:
         data = request.POST
-        pprint(data)
         MyDb.objects.create(username=data["username"], email=data["email"])
         return HttpResponse("Added successfully!")
 
@@ -26,17 +24,24 @@ class Add(View):
 class Delete(View):
     def post(self, request: WSGIRequest) -> HttpResponse:
         data = request.POST
-        MyDb.objects.filter(id=data["id"]).delete()
+        try:
+            result = MyDb.objects.get(id=data["id"])
+            result.delete()
+        except MyDb.DoesNotExist:
+            raise Http404(f"Item {data['id']} Not Found!")
         return HttpResponse("Deleted successfully")
 
 
 class Update(View):
     def post(self, request: WSGIRequest) -> HttpResponse:
         data = request.POST
-        record = MyDb.objects.get(id=data["id"])
-        if len(data["username"]) != 0:
-            record.username = data["username"]
-        elif len(data["email"]) != 0:
-            record.email = data["email"]
-        record.save()
+        try:
+            record = MyDb.objects.get(id=data["id"])
+            if len(data["username"]) != 0:
+                record.username = data["username"]
+            if len(data["email"]) != 0:
+                record.email = data["email"]
+            record.save()
+        except MyDb.DoesNotExist:
+            raise Http404(f"Item {data['id']} Not Found!")
         return HttpResponse("Updated Successfully")
