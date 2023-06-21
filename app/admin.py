8b6760e4418b03bd0b292c97f14b5056900157f2
wingxel <wingxel@gmail.com>
2023-06-21 17:58:45 +0300

    Added list_display

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import MyDb
 
 
-admin.site.register(MyDb)
+class MyDbAdmin(admin.ModelAdmin):
+	list_display = ["username", "email"]
+
+
+admin.site.register(MyDb, MyDbAdmin)
